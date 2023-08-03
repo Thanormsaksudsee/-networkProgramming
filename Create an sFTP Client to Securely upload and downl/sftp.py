c5654de223bf6f01820229c7e46c65daf02a3710
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2023-08-03 10:34:33 +0700

    cz

--- Create an sFTP Client to Securely upload and downl/sftp.py
+++ Create an sFTP Client to Securely upload and downl/sftp.py
@@ -1,6 +1,6 @@
 import paramiko
 
-hostname = '192.168.63.129'
+hostname = '192.168.52.1'
 username = "bigkuma"
 passwd = " "
 port = 22
@@ -11,7 +11,7 @@ try:
     print("[*] Connected to " + hostname + " via SSH")
     sftp = paramiko.SFTPClient.from_transport(p)
     print("[*] Starting file download")
-    sftp.get("/home/bigkuma/test.txt","Users/bigkuma/Downloads/d.txt")
+    sftp.get("/home/bigkuma/test.txt","Users/KMUTNB/Desktop/d.txt")
     print("[*] File downloaded complete")
     print("[*] Starting file upload")
     sftp.put("/Users/bigkuma/Downloads/d.txt","/home/bigkuma/u.txt")
